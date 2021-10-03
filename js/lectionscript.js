85c88a7894e2e9e38a5ecefcb93cd903210b01c5
Tanya <tetyana.a.zaliska@gmail.com>
2021-10-04 01:48:07 +0300

    tested cicles and iterations

--- js/lectionscript.js
+++ js/lectionscript.js
@@ -19,19 +19,42 @@ if (4 === 4) {
 // (num === 50) ? console.log("Ok!") : console.log("Error!");
 
 
-const num = 50;
+// const num = 50;
 
-//switch - строгое равенство ===
-switch (num) {
-    case 49: 
-        console.log("Incorrect!");
-        break;
-    case 100:
-        console.log("Incorrect!");
-        break;
-    case "50": // этот код не выполнится так как "50" - это строка
-        console.log("Exactly!");
-        break;
-    default:
-        console.log("Not this time");
+// //switch - строгое равенство ===
+// switch (num) {
+//     case 49: 
+//         console.log("Incorrect!");
+//         break;
+//     case 100:
+//         console.log("Incorrect!");
+//         break;
+//     case "50": // этот код не выполнится так как "50" - это строка
+//         console.log("Exactly!");
+//         break;
+//     default:
+//         console.log("Not this time");
+// }
+
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// } 
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+
+    console.log(i); 
 }
