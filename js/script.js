aec796237908ff2f4698186c25b97d969d1ca0ae
Tanya <tetyana.a.zaliska@gmail.com>
2021-10-06 01:58:28 +0300

    practice part 3 - functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-'Просмотрено довольно мало фильмов', если от 10 до 30 - 'Вы классический зритель', а если больше - 
-'Вы киноман'. А если не подошло ни к одному варианту - 'Произошла ошибка'
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-const numberOfFilms = +prompt('How many films do you already watch?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films do you already watch?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films do you already watch?', '');
+    }
+}
+ 
+
+//start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -24,107 +34,53 @@ const personalMovieDB = {
     private: false
 };
 
-let askQuestion1 = prompt('One of last wached movies?', ''),
-      rate1 = +prompt('How can you rate it?', ''),
-      askQuestion2 = prompt('One of last wached movies?', ''),
-      rate2 = +prompt('How can you rate it?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const askQuestion = prompt('One of last wached movies?', ''),
+              rate = prompt('How can you rate it?', ''); 
+    
+        if ((askQuestion !== '') && (askQuestion !== null) && (askQuestion.length <= 50) &&
+            (rate !== '') && (rate !== null)) {
+            personalMovieDB.movies[askQuestion] = rate;
+        } else {
+            i--;
+        } 
+    }
+}
 
-personalMovieDB.movies[askQuestion1] = rate1;
-personalMovieDB.movies[askQuestion2] = rate2;
-// personalMovieDB.movies = {
-//     askQuestion1: rate1,
-//     askQuestion2: rate2
-// };
+//rememberMyFilms();
 
-for (let i = 0; i < 2; i++) {
-    const askQuestion = prompt('One of last wached movies?', ''),
-          rate = prompt('How can you rate it?', ''); 
-
-    if ((askQuestion !== '') && (askQuestion !== null) && (askQuestion.length <= 50) &&
-        (rate !== '') && (rate !== null)) {
-        personalMovieDB.movies[askQuestion] = rate;
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Watched to small movies'); 
+    } else if ((personalMovieDB.count <= 30) && (personalMovieDB.count >= 10)) {
+        console.log('You are a classic viewer'); 
+    } else if (personalMovieDB.count > 30) {
+        console.log('You are a movie fan');
     } else {
-        i--;
-    } 
+        console.log('Error occured');
+    }
 }
 
-let i = 0;
+//detectPersonalLevel();
 
-while (i < 2) {
-    const askQuestion = prompt('One of last wached movies?', ''),
-          rate = prompt('How can you rate it?', ''); 
-
-    if ((askQuestion !== '') && (askQuestion !== null) && (askQuestion.length <= 50) &&
-        (rate !== '') && (rate !== null)) {
-        personalMovieDB.movies[askQuestion] = rate;
-        i++;
-    } 
+function showMyDB() {
+    if (personalMovieDB.private) {
+        return ;
+    }
+    console.log(personalMovieDB);
 }
 
-//let i = 0;
+showMyDB();
 
-do {
-    const askQuestion = prompt('One of last wached movies?', ''),
-          rate = prompt('How can you rate it?', ''); 
-
-    if ((askQuestion !== '') && (askQuestion !== null) && (askQuestion.length <= 50) &&
-        (rate !== '') && (rate !== null)) {
-        personalMovieDB.movies[askQuestion] = rate;
-        i++;
-    } 
-}
-while (i < 2);
-
-
-// let askQuestion;
-// let rate;
-
-// for (let i = 0; i < 2; i++) {
-//     let askQuestion = prompt('One of last wached movies?', ''),
-//         rate = +prompt('How can you rate it?', '');
-
-//     while (true) {
-//         if ((askQuestion !== '') && (askQuestion !== null) && (askQuestion.length <= 50) &&
-//             (rate !== '') && (rate !== null)) {
-//             break;
-//         } 
-//         askQuestion = prompt('One of last wached movies?', '');
-//         rate = +prompt('How can you rate it?', '');
-//     }
-//     personalMovieDB.movies[askQuestion] = rate;
-// }
-
-// for (let i = 0; i < 2; i++) {
-//     do {
-//         askQuestion = prompt('One of last wached movies?', '');
-//         if ((askQuestion !== '')&&(askQuestion !== null)&&(askQuestion.length <= 50)) {
-//             break;
-//         } 
-//     } 
-//     while (true);
-//     rate = +prompt('How can you rate it?', '');
-//     personalMovieDB.movies[askQuestion] = rate;
-// }
-
-// for (let i = 0; i < 2; i++) {
-//     for (;true;) {
-//         askQuestion = prompt('One of last wached movies?', '');
-//         if ((askQuestion !== '')&&(askQuestion !== null)&&(askQuestion.length <= 50)) {
-//             break;
-//         } 
-//     } 
-//     rate = +prompt('How can you rate it?', '');
-//     personalMovieDB.movies[askQuestion] = rate;
-// }
-
-if (personalMovieDB.count < 10) {
-    console.log('Watched to small movies'); 
-} else if ((personalMovieDB.count <= 30) && (personalMovieDB.count >= 10)) {
-    console.log('You are a classic viewer'); 
-} else if (personalMovieDB.count > 30) {
-    console.log('You are a movie fan');
-} else {
-    console.log('Error occured');
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Your favorite genre is ${ i+1 }`, '');
+        if (personalMovieDB.genres[i] == '' || personalMovieDB.genres[i] == null) { 
+            i--;
+        }
+    }
 }
 
-console.log(personalMovieDB);
+writeYourGenres();
+
