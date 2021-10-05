36b8eae29285e4d3a5d59a41b17b7c6d27c06ac3
Tanya <tetyana.a.zaliska@gmail.com>
2021-10-06 02:25:10 +0300

    callback function lesson done

--- js/lectionscript.js
+++ js/lectionscript.js
@@ -1,80 +1,29 @@
 "use strict";
 
-let num = 10;
-
-//Function Declaration
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 20;
-    console.log(num);
+function first() {
+    //do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+function second() {
+    console.log(2);
 }
 
-showFirstMessage('Hello world!');
-console.log(num);
+first();
+second();
 
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
-
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-function ret() {
-    let num = 15;
-    return num; 
+function learnJS(lang, callback) {
+    console.log(`I learn ${lang}`);
+    callback();
 }
 
-const anotherNum = ret();
-console.log(anotherNum);
+// learnJS('JavaScript', function() {
+//     console.log('I learned this lesson!');
+// });
 
-//Function Expression
-const logger = function() {
-    console.log('Hello');
-};
+function done() {
+    console.log('I learned this lesson!');
+}
 
-logger();
-
-//Стрелочные функции
-const calc = (a, b) => a + b;
-//const calc = (a, b) => { 
-//    console.log('1');
-//    return a + b; 
-//}
-//const double = a => a * 2;
-//const noParams = () => 5 * 2;
-
-//lesson 14
-
-const str = 'test';
-const arr = [1, 2, 4];
-
-//console.log(arr.length);
-
-//console.dir(Number);
-
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str);
-
-const fruit = 'Some fruitu';
-console.log(fruit.indexOf('u'));
-
-const logg = 'Hello world'; //total 10 characters
-
-console.log(logg.slice(6, 10));//вырезать от символа х до символа у
-console.log(logg.slice(6));
-console.log(logg.slice(-4, -1));
-
-console.log(logg.substring(6, 10)); //как slice, только без отрицательных, можно первый арг больше чем второй
-
-console.log(logg.substr(6, 4));//вырезать с позиции х количество символов нум
-
-
-const numb = 12.2;
-console.log(Math.round(numb));
-
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
-
+learnJS('JavaScript', done);
