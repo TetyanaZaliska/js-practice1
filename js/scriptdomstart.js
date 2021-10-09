ba5a79eff60996df6fc3d4a498d562ed8d262597
Tanya <tetyana.a.zaliska@gmail.com>
2021-10-10 01:51:33 +0300

    updated dom select elements

--- js/scriptdomstart.js
+++ js/scriptdomstart.js
@@ -6,12 +6,14 @@ console.log(box);
 
 const btns = document.getElementsByTagName('button');
 const btn = document.getElementsByTagName('button')[1];
-const btn2 = btns[2];
+const btn2 = btns[0];
+console.log(btns);
 
 
 const circles = document.getElementsByClassName('circle');
 const circle = document.getElementsByClassName('circle')[0];
 const circle2 = circles[2];
+console.log(circles[0]);
 
 
 const hearts = document.querySelectorAll('.heart');
