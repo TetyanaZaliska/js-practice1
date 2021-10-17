fd24c100cdcb4583de4fc52c3e29971a9766b019
Tanya <tetyana.a.zaliska@gmail.com>
2021-10-18 01:05:39 +0300

    task 30 with eventistener done

--- js/script2.js
+++ js/script2.js
@@ -1,68 +1,139 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    
+    
+    // 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+    const adv = document.querySelectorAll('div.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'), 
+          addInput = addForm.querySelector('input.adding__input'),
+          addCheckBox = addForm.querySelector('input[type="checkbox"]');
+    
+
+    
+    
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let newMovie = addInput.value;
+        const isFavourite = addCheckBox.checked;
+
+        if (!newMovie) {
+            return;
+        }
+    
+        if (newMovie.length > 21) { 
+            // newMovie = newMovie.slice(0, 21) + '...';
+            newMovie = `${newMovie.substring(0, 22)}...`;
+        } 
+    
+        if (isFavourite) { 
+            console.log('Add this movie to your favourite');
+        }
+    
+        movieDB.movies.push(newMovie); 
+
+        createMovieList(movieDB.movies, movieList); 
+
+        event.target.reset();
+    });
 
 
+    const deleteAdv = (arr) => {
+        arr.forEach(item => item.remove());
+    }; 
+    
+    const makeChanges = () => {
+        genre.textContent = 'драма'; 
 
-// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
-const adv = document.querySelectorAll('div.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
+        poster.style.backgroundImage = 'url("../img/bg.jpg")';
+    };
 
-adv.forEach(item => item.remove());
+    const sortArr = (arr) => {
+        arr.sort();
+    };  
+    
+    function createMovieList(films, parent) {
+        parent.innerHTML = ''; 
 
-// adv.forEach(item => {
-//     item.remove();
-// });
+        sortArr(films); 
 
-// adv.forEach(function(item) {
-//     item.remove();
-// });
+        films.forEach((film, i) => {
+            // movieList.insertAdjacentHTML('beforeend', `<li class="promo__interactive-item">${i+1} ${film}
+            //         <div class="delete"></div>
+            //     </li>`);
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>`;
+        });
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', (ev) => {
+                ev.preventDefault(); 
+                btn.parentElement.remove();
+                films.splice(i, 1); 
 
-// 2) Изменить жанр фильма, поменять "комедия" на "драма"
-genre.textContent = 'драма';
+                createMovieList(films, parent);
+            }); 
+        });
+    }
+    
+    // function addMovieEvent() {
+    //     movieList.querySelectorAll('.delete').forEach(element => {
+    //         element.addEventListener('click', (ev) => {
+    //             ev.preventDefault();
+    //             // console.log(ev);
+    //             let deleteMovieName = element.parentElement.textContent.trim();
+    //             deleteMovieName = deleteMovieName.substr(2, deleteMovieName.length-2); 
+    //             const deleteMoviePos = movieDB.movies.indexOf(deleteMovieName); 
+    //             movieDB.movies.splice(deleteMoviePos, 1);
+    //             element.parentElement.remove();
+    //             console.log(ev.target);
+    //         }); 
+    //     });
+    // }
 
-// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-// Реализовать только при помощи JS
-// poster.style.background = 'url(../img/bg.jpg) center center/cover no-repeat'; 
-poster.style.backgroundImage = 'url("../img/bg.jpg")';
+    
 
-// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-// Отсортировать их по алфавиту 
-// 5) Добавить нумерацию выведенных фильмов
- 
-movieList.innerHTML = '';
-
-movieDB.movies.sort().forEach((film, i) => {
-    // movieList.insertAdjacentHTML('beforeend', `<li class="promo__interactive-item">${i+1} ${film}
-    //         <div class="delete"></div>
-    //     </li>`);
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>`;
+    deleteAdv(adv); 
+    makeChanges();  
+    createMovieList(movieDB.movies, movieList); 
+    
+    
+    
 });
+
+
+
+
+
 
